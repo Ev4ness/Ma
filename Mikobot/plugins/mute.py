ea6559abeb7139c4f494c1a7234c5966f5ba36f8
Vooyage <148488760+Vygaee@users.noreply.github.com>
2024-02-17 15:08:04 +0700

    Update mute.py

--- Mikobot/plugins/mute.py
+++ Mikobot/plugins/mute.py
@@ -256,6 +256,6 @@ function(MUTE_HANDLER)
 function(UNMUTE_HANDLER)
 function(TEMPMUTE_HANDLER)
 
-__mod_name__ = "MUTE"
+__mod_name__ = "Mute"
 __handlers__ = [MUTE_HANDLER, UNMUTE_HANDLER, TEMPMUTE_HANDLER]
 # <================================================ END =======================================================>
